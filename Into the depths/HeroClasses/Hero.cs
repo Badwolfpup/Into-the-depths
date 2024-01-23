881ba3967c1a618a05c861170ded2ae702988941
Badwolfpup <Adam.Folke@hudiksvall.se>
2024-01-23 13:43:03 +0100

    Add project files.

---
+++ Into the depths/HeroClasses/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Into_the_depths.HeroClasses
+{
+    public abstract class Hero
+    {
+        public abstract int Strength { get; set; }
+        public abstract int Agility { get; set; }
+        public abstract int Intellect { get; set; }
+        public abstract int Spirit { get; set; }
+
+        public abstract int Stamina { get; set; }
+
+        public abstract int Armor { get; set; }
+        public abstract int PhysicalDefense { get; set; }
+        public abstract int MagicDefense { get; set; }
+
+        public abstract int FireResist { get; set; }
+        public abstract int ColdResist { get; set; }
+        public abstract int PoisonResist { get; set; }
+        public abstract int ArcaneResist { get; set; }
+
+
+    }
+}
---
+++ Into the depths/HeroClasses/Paladin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Into_the_depths.HeroClasses
+{
+    internal class Paladin : Hero
+    {
+        public override int Strength { get; set; }
+        public override int Agility { get; set; }
+        public override int Intellect { get; set; }
+        public override int Spirit { get; set; }
+        public override int Stamina { get; set; }
+        public override int Armor { get; set; }
+        public override int PhysicalDefense { get; set; }
+        public override int MagicDefense { get; set; }
+        public override int FireResist { get; set; }
+        public override int ColdResist { get; set; }
+        public override int PoisonResist { get; set; }
+        public override int ArcaneResist { get; set; }
+    }
+}
